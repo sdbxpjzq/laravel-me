871a259602021fe1ecd6b1e55e62b8ce195ca1aa
zongqi <1165064143@qq.com>
2018-11-08 09:06:06 +0800

    swoole 开始

---
+++ app/Http/Controllers/Swoole/Client/TCPClient.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zongqi
+ * Date: 2018/11/7
+ * Time: 下午9:28
+ */
+
+namespace App\Http\Controllers\Swoole\Client;
+
+
+class TCPClient
+{
+    public static function TCPClient()
+    {
+        $client = new \swoole_client(SWOOLE_SOCK_TCP);
+        if (!$client->connect('127.0.0.1', 9501)) {
+            echo '连接失败';
+            exit;
+        }
+
+        fwrite(STDOUT, '请输入');
+        $msg = trim(fgets(STDIN));
+        // 发送给 server
+        $client->send($msg);
+        // 接受server 端数据
+        $ret = $client->recv();
+        echo $ret;
+    }
+}
+
+TCPClient::TCPClient();
---
+++ app/Http/Controllers/Swoole/Server/http_server.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zongqi
+ * Date: 2018/11/8
+ * Time: 上午5:56
+ */
+
+namespace App\Http\Controllers\Swoole\Server;
+
+
+class http_server
+{
+    public static function http()
+    {
+
+        $http = new \swoole_http_server("0.0.0.0", 8811);
+        $http->set([
+//            设置document_root并设置enable_static_handler为true后，
+//底层收到Http请求会先判断document_root路径下是否存在此文件，
+//如果存在会直接发送文件内容给客户端，不再触发onRequest回调。
+            'document_root' => '/data/wwwroot/laravel/app/Http/Controllers/Swoole/Data',
+            'enable_static_handler' => true,
+        ]);
+        $http->on('request', function (\swoole_http_request $request, \swoole_http_response $response) {
+            // 发送给浏览器
+            print_r($request->get);
+            $response->end("<h1>Hello Swoole. #".mt_rand(1000, 9999)."</h1>");
+        });
+
+        $http->start();
+    }
+}
+http_server::http();
